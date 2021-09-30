fa9bcb4757e05c105904452c63e99beec1ed3b36
lpr78 <lukeprice@blueyonder.co.uk>
2021-09-30 12:53:13 +0000

    adding multiply element

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,8 +30,12 @@ function runGame(gameType) {
     let num2 = Math.floor(Math.random() * 25) + 1
 
     if(gameType === "addition"){
-        displayAdditionQuestion(num1,num2)
-    } else {
+        displayAdditionQuestion(num1,num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1,num2);
+    }
+    
+    else {
         alert(`Unknown Game Type ${gameType}`);
         throw `Unknown Game Type ${gameType}.Aborting!`;
     }
@@ -68,7 +72,10 @@ function calculateCorrectAnswer() {
 
     if(operator === "+"){
         return[operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    }
+    else {
         alert(`The operator ${operator} does not exist`);
         throw `The operator ${operator} does not exist.Aborting!`;
     }
@@ -102,12 +109,17 @@ function displayAdditionQuestion(operand1,operand2) {
 
 function displaySubtractQuestion() {
 
+
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1,operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
 function displayDivideQuestion() {
+
     
 }
